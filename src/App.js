f04de7eb716964fb1a88db870cc1583e2125fb35
kvenus-uj <alexstepan0209@gmail.com>
2022-08-11 12:58:30 +0300

    'first'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button, Col, Row, Dropdown, DropdownButton, Form } from "react-bootstrap";
+import { useRef, useState } from 'react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 function App() {
+  const amount = useRef();
+  const [side, setSide] = useState("FRONT");
+  function onChange(val){
+
+    if(val==="front"){
+      setSide("FRONT");
+    } else if(val==="back"){
+      setSide("BACK");
+    }
+    
+  }
+  const bet = async () => {
+    console.log('bet: ', amount.current.value);
+  }
+  const start = async () => {
+    console.log('Start betting...');
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <WalletMultiButton className="wallet-btn"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is simple betting test UI.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Row>
+        <Col>
+          <Form.Control
+            type="number"
+            ref={amount}
+            placeholder="SOL for Betting"
+          />
+        </Col>
+        <Col>
+          <Button
+            type="submit"
+            onClick={bet}
+          >Bet
+          </Button>
+        </Col>
+        <Col>
+          <DropdownButton title={side} onSelect={(evt) => onChange(evt)}>
+            <Dropdown.Item eventKey="front">FRONT</Dropdown.Item>
+            <Dropdown.Item eventKey="back">BACK</Dropdown.Item>
+          </DropdownButton>
+        </Col>
+        <Col>
+          <Button
+            type="submit"
+            onClick={start}
+          >Start!
+          </Button>
+        </Col>
+      </Row>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
+import WalletConnectionProvider from './wallet_connection';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WalletConnectionProvider>
+      <App />
+    </WalletConnectionProvider>
   </React.StrictMode>
 );
 
